14d66bbdf780bdfc32d1176b8250e28bab52a91a
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-08-27 13:44:23 -0300

    Correção do primeiro commit

---
+++ src/test/java/com/pombo/pombo/PomboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pombo.pombo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
