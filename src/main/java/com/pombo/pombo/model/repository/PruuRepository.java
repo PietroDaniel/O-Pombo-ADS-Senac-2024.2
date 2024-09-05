6d56f7d65a205a513fe206ed99c502857975bd00
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-09-05 10:20:42 -0300

    Repositories atualizados

--- src/main/java/com/pombo/pombo/model/repository/PruuRepository.java
+++ src/main/java/com/pombo/pombo/model/repository/PruuRepository.java
@@ -1,5 +1,11 @@
 package com.pombo.pombo.model.repository;
 
-public class PruuRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+import com.pombo.pombo.model.entity.Pruu;
+
+@Repository
+public interface PruuRepository extends JpaRepository<Pruu, Long>, JpaSpecificationExecutor<Pruu> {
     
 }
--- src/main/java/com/pombo/pombo/model/repository/UsuarioRepository.java
+++ src/main/java/com/pombo/pombo/model/repository/UsuarioRepository.java
@@ -2,8 +2,10 @@ package com.pombo.pombo.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 import com.pombo.pombo.model.entity.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>, JpaSpecificationExecutor<Usuario> {
     
 }
