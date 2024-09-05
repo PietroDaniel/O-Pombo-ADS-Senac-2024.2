6597ab9a33f927fe910883d7fab54ed4f09355e8
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-09-05 10:00:39 -0300

    BaseSeletor e pombo_db atualizados

--- src/main/java/com/pombo/pombo/model/seletor/BaseSeletor.java
+++ src/main/java/com/pombo/pombo/model/seletor/BaseSeletor.java
@@ -1,5 +1,14 @@
 package com.pombo.pombo.model.seletor;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.Data;
+
+@Data
 public abstract class BaseSeletor {
 
     private int pagina;
@@ -17,7 +26,37 @@ public abstract class BaseSeletor {
     public int getOffset() {
         return this.limite * (this.pagina - 1);
     }
+
+    public static void aplicarFiltroPeriodo(Root<?> root, 
+            CriteriaBuilder cb, List<Predicate> predicates,
+            Integer valorMinimo, Integer valorMaximo, String nomeAtributo) {
+          if (valorMinimo != null && valorMaximo != null) {
+              // WHERE atributo BETWEEN min AND max
+              predicates.add(cb.between(root.get(nomeAtributo), valorMinimo, valorMaximo));
+          } else if (valorMinimo != null) {
+              // WHERE atributo >= min
+              predicates.add(cb.greaterThanOrEqualTo(root.get(nomeAtributo), valorMinimo));
+          } else if (valorMaximo != null) {
+              // WHERE atributo <= max
+              predicates.add(cb.lessThanOrEqualTo(root.get(nomeAtributo), valorMaximo));
+          }
+    }
     
+    public static void aplicarFiltroPeriodo(Root<?> root, 
+            CriteriaBuilder cb, List<Predicate> predicates,
+            LocalDate dataInicial, LocalDate dataFinal, String nomeAtributo) {
+          if (dataInicial != null && dataFinal != null) {
+              // WHERE atributo BETWEEN min AND max
+              predicates.add(cb.between(root.get(nomeAtributo), dataInicial, dataFinal));
+          } else if (dataInicial != null) {
+              // WHERE atributo >= min
+              predicates.add(cb.greaterThanOrEqualTo(root.get(nomeAtributo), dataInicial));
+          } else if (dataFinal != null) {
+              // WHERE atributo <= max
+              predicates.add(cb.lessThanOrEqualTo(root.get(nomeAtributo), dataFinal));
+          }
+    }
+
     public int getPagina() {
         return pagina;
     }
