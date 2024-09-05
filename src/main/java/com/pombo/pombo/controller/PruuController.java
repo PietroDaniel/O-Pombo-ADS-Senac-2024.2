9ca194445fad8e1aab3726315c4ff62264b7e45e
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-09-05 10:35:22 -0300

    Controllers atualizados

--- src/main/java/com/pombo/pombo/controller/PruuController.java
+++ src/main/java/com/pombo/pombo/controller/PruuController.java
@@ -1,5 +1,48 @@
 package com.pombo.pombo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.pombo.pombo.model.entity.Pruu;
+import com.pombo.pombo.service.PruuService;
+import com.pombo.pombo.exception.PomboException;
+
+@RestController
+@RequestMapping("/api/pruus")
 public class PruuController {
-    
+
+    @Autowired
+    private PruuService pruuService;
+
+    @GetMapping
+    public List<Pruu> listarTodos() {
+        return pruuService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pruu> buscarPorId(@PathVariable Long id) throws PomboException {
+        Pruu pruu = pruuService.buscarPorId(id);
+        return ResponseEntity.ok(pruu);
+    }
+
+    @PostMapping
+    public ResponseEntity<Pruu> criarPruu(@RequestBody Pruu novoPruu) {
+        Pruu pruuCriado = pruuService.criarPruu(novoPruu);
+        return ResponseEntity.status(201).body(pruuCriado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pruu> atualizarPruu(@PathVariable Long id, @RequestBody Pruu pruuAtualizado) throws PomboException {
+        Pruu pruu = pruuService.atualizarPruu(id, pruuAtualizado);
+        return ResponseEntity.ok(pruu);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPruu(@PathVariable Long id) throws PomboException {
+        pruuService.excluirPruu(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/pombo/pombo/controller/UsuarioController.java
+++ src/main/java/com/pombo/pombo/controller/UsuarioController.java
@@ -1,5 +1,48 @@
 package com.pombo.pombo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.pombo.pombo.model.entity.Usuario;
+import com.pombo.pombo.service.UsuarioService;
+import com.pombo.pombo.exception.PomboException;
+
+@RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
-    
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> listarTodos() {
+        return usuarioService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) throws PomboException {
+        Usuario usuario = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario novoUsuario) {
+        Usuario usuarioCriado = usuarioService.criarUsuario(novoUsuario);
+        return ResponseEntity.status(201).body(usuarioCriado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioAtualizado) throws PomboException {
+        Usuario usuario = usuarioService.atualizarUsuario(id, usuarioAtualizado);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) throws PomboException {
+        usuarioService.excluirUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
 }
