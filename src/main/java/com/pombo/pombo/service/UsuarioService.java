562dde3bf763b4fa3fe821f5845f74bfffa38b7e
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-09-15 17:55:26 -0300

    UsuarioService atualizado

--- src/main/java/com/pombo/pombo/service/UsuarioService.java
+++ src/main/java/com/pombo/pombo/service/UsuarioService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pombo.pombo.exception.PomboException;
+import com.pombo.pombo.model.entity.Pruu;
 import com.pombo.pombo.model.entity.Usuario;
+import com.pombo.pombo.model.repository.PruuRepository;
 import com.pombo.pombo.model.repository.UsuarioRepository;
 
 @Service
@@ -14,6 +16,9 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PruuRepository pruuRepository;
 
     public List<Usuario> listarTodos() {
         return usuarioRepository.findAll();
@@ -39,5 +44,31 @@ public class UsuarioService {
     public void excluirUsuario(Long id) throws PomboException {
         Usuario usuario = buscarPorId(id);
         usuarioRepository.delete(usuario);
+    }
+
+    public void likePruu(Long usuarioId, Long pruuId) throws PomboException {
+        Usuario usuario = buscarPorId(usuarioId);
+        Pruu pruu = pruuRepository.findById(pruuId)
+            .orElseThrow(() -> new PomboException("Pruu não encontrado"));
+
+        if (!usuario.getLikedPruus().contains(pruu)) {
+            usuario.getLikedPruus().add(pruu);
+            pruu.setQuantidadeLikes(pruu.getQuantidadeLikes() + 1);
+            usuarioRepository.save(usuario);
+            pruuRepository.save(pruu);
+        }
+    }
+
+    public void unlikePruu(Long usuarioId, Long pruuId) throws PomboException {
+        Usuario usuario = buscarPorId(usuarioId);
+        Pruu pruu = pruuRepository.findById(pruuId)
+            .orElseThrow(() -> new PomboException("Pruu não encontrado"));
+
+        if (usuario.getLikedPruus().contains(pruu)) {
+            usuario.getLikedPruus().remove(pruu);
+            pruu.setQuantidadeLikes(pruu.getQuantidadeLikes() - 1);
+            usuarioRepository.save(usuario);
+            pruuRepository.save(pruu);
+        }
     }
 }
