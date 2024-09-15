51eb3126ab1f06ad770805260d8dbb17bca3fa37
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-09-15 17:49:30 -0300

    entidade Pruu com many-to-many

--- src/main/java/com/pombo/pombo/model/entity/Pruu.java
+++ src/main/java/com/pombo/pombo/model/entity/Pruu.java
@@ -1,18 +1,12 @@
 package com.pombo.pombo.model.entity;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.annotations.UuidGenerator;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -50,4 +44,7 @@ public class Pruu {
     @ManyToOne
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
+
+    @ManyToMany(mappedBy = "likedPruus")
+    private List<Usuario> likedByUsers = new ArrayList<>();
 }
