7d4fa18d65da695b20ae8e6fb231b010a7c34856
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-10-16 16:43:39 -0300

    appproperties e tests e pomboapptests.java

---
+++ src/test/java/com/pombo/pombo/PomboApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pombo.pombo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomboApplicationTests {
+
+    @Test
+    void contextLoads() {
+       
+
+    }
+}
