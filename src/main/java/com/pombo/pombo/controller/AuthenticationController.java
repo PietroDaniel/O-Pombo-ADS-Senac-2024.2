9fde5eca2b74c5f5fa1e6cf06310aba7aaa228a7
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-10-31 08:59:42 -0300

    Create AuthenticationController, ImagemController e ImagemService

---
+++ src/main/java/com/pombo/pombo/controller/AuthenticationController.java
@@ -0,0 +1,56 @@
+package br.sc.senac.vemnox1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.sc.senac.vemnox1.auth.AuthenticationService;
+import br.sc.senac.vemnox1.model.entity.Jogador;
+import br.sc.senac.vemnox1.model.enums.PerfilAcesso;
+import br.sc.senac.vemnox1.service.JogadorService;
+
+@RestController
+@RequestMapping(path = "/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private JogadorService jogadorService;
+
+    /**
+     * Método de login padronizado -> Basic Auth
+     * 
+     *  O parâmetro Authentication já encapsula login (username) e senha (password)
+     *  Basic <Base64 encoded username and password>
+     * @param authentication
+     * @return o JWT gerado
+     */
+    @PostMapping("authenticate")
+    public String authenticate(Authentication authentication) {
+        return authenticationService.authenticate(authentication);
+    }
+    
+    @PostMapping("/novo-jogador")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void registrarJogador(@RequestBody Jogador novoJogador) {
+    	
+      String senhaCifrada = passwordEncoder.encode(novoJogador.getSenha()); 	
+    	
+      novoJogador.setSenha(senhaCifrada);
+      novoJogador.setPerfil(PerfilAcesso.JOGADOR);
+      
+      jogadorService.inserir(novoJogador);
+    }
+      
+}
