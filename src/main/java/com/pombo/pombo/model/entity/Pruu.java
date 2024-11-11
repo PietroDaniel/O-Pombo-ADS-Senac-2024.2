ccfe203a23c2db862eeb63f74aa91ef231b07838
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-11-11 19:40:43 -0300

    Update Pruu.java

--- src/main/java/com/pombo/pombo/model/entity/Pruu.java
+++ src/main/java/com/pombo/pombo/model/entity/Pruu.java
@@ -4,17 +4,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.UuidGenerator;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.OneToMany; // Adicione essa importação
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -22,25 +14,12 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "pruus")
-public class Pruu {
 
+public class Pruu {
     @Id 
 	@UuidGenerator
-	private String uuid; 
-
-    @NotBlank(message = "O texto do pruu é obrigatório")
-    @Size(min = 1, max = 300, message = "O texto deve ter entre 1 e 300 caracteres")
-    @Column(name = "texto", nullable = false)
-    private String texto;
-
-    @Column(name = "data_hora_criacao", nullable = false, updatable = false)
-    private LocalDateTime dataHoraCriacao = LocalDateTime.now();
-
-    @Column(name = "bloqueado", nullable = false)
-    private boolean bloqueado = false;
-
-    @Column(name = "imagem")
-    private String imagem;
+    @GeneratedValue(strategy = GenerationType.UUID)
+	private String uuid;
 
     @ManyToOne
     @JoinColumn(name = "usuario_id", nullable = false)
@@ -51,5 +30,21 @@ public class Pruu {
     private List<Usuario> likedByUsers = new ArrayList<>();
 
     @OneToMany(mappedBy = "pruu")
-    private List<Denuncia> denuncias = new ArrayList<>(); 
+    private List<Denuncia> denuncias = new ArrayList<>();
+
+    @NotBlank(message = "O texto do pruu é obrigatório")
+    @Size(min = 1, max = 350, message = "O texto deve ter entre 1 e 350 caracteres")
+    private String texto;
+
+    @Column(name = "imagem")
+    private String imagem;
+
+    private boolean bloqueado = false;
+    private boolean excluido = false;
+
+    @Column(name = "data_hora_criacao", nullable = false, updatable = false)
+    private LocalDateTime dataHoraCriacao = LocalDateTime.now();
+
+
+
 }
