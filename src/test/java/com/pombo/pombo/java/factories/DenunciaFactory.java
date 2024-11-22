1defbd3fea3515712e06120ac3d2f33dcc3b5f74
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-11-22 09:25:03 -0300

    Update DenunciaFactory e DenunciaRepoTest

--- src/test/java/com/pombo/pombo/java/factories/DenunciaFactory.java
+++ src/test/java/com/pombo/pombo/java/factories/DenunciaFactory.java
@@ -1,4 +1,19 @@
 package com.pombo.pombo.java.factories;
 
+import com.pombo.pombo.model.entity.Denuncia;
+import com.pombo.pombo.model.entity.Pruu;
+import com.pombo.pombo.model.entity.Usuario;
+import com.pombo.pombo.model.enums.MotivoDenuncia;
+
 public class DenunciaFactory {
+
+    public static Denuncia criarDenuncia(Usuario denunciante, Pruu pruu, MotivoDenuncia motivo) {
+        Denuncia denuncia = new Denuncia();
+
+        denuncia.setDenunciante(denunciante);
+        denuncia.setPruu(pruu);
+        denuncia.setMotivo(motivo);
+
+        return denuncia;
+    }
 }
--- src/test/java/com/pombo/pombo/java/model/repository/DenunciaRepositoryTest.java
+++ src/test/java/com/pombo/pombo/java/model/repository/DenunciaRepositoryTest.java
@@ -1,4 +1,5 @@
 package com.pombo.pombo.java.model.repository;
 
 public class DenunciaRepositoryTest {
+
 }
